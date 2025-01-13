d212ee41cf344947731f650d6a91dbd5062cf5a6
amityohan <amit.yohan@kalvium.community>
2025-01-13 14:22:25 +0530

    created a homepage

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Me, Myself and I</h1>
+      <div>
+        <p>Name: Amit Yohan Horo a.k.a. just Yohan</p>
+      </div>
+      <h2> Contact Details</h2>
+      <p className='contact-details'>Phone no:65955698655</p>
+      <p>Email: memyselfandi@gmail.com</p>
+
+    </div>
+  )
+}
+
+export default App
